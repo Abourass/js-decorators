59c2f910e601a134f6259f1b3f13474f129d7136
Abourass <abourassa@assetval.com>
2020-02-12 13:07:39 -0700

    :tada: Make it so number one

---
+++ delayingDecorator.js
@@ -0,0 +1,13 @@
+const saySomething = x => console.log(x);
+
+function delay(fn, ms){
+  return function(){
+    setTimeout(() => fn.apply(this, arguments), ms)
+  }
+}
+
+const sayHiAfter1000ms = delay(saySomething, 1000);
+const sayHelloAfter2000ms = delay(saySomething, 2000);
+
+sayHiAfter1000ms('Hi');
+sayHelloAfter2000ms('Hello');
